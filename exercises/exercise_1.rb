1e15d8e4f6a87db6fc23a72b8edfd0bd792112c2
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-11 02:18:25 +0000

    Ran some conditional selections

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
-richmond = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby = Store.create(name: "burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
+richmond = Store.create(name: "richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name}, Annual Revnue: $#{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womens_stores.each do |store|
+  puts "Store Name: #{store.name}, Annual Revnue: $#{store.annual_revenue}"
+end
