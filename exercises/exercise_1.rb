9cf0962d483606fd20b7497ce96ca11218f6206d
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-11 01:31:41 +0000

    Added three records to the db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  self.table_name = "stores"
+end
+
+burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
+richmond = Store.create(annual_revenue: 1260000, mens_apparel: nil, womens_apparel: true)
+gastown = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
