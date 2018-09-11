af10a01f7e63323582763da2fda57021f4a63bd7
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-11 19:41:31 +0000

    Added before_destroy check for number of employees

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,14 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates_length_of :name, :minimum => 5
   validates_numericality_of :annual_revenue, :only_integer => true, :greater_than_or_equal_to => 0
+
+  before_destroy :check_number_of_employees
+
+  private
+
+  def check_number_of_employees
+    if self.employees.sum(:id) > 0
+      throw :abort
+    end
+  end
 end
