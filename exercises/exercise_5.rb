b7f485c70e311225cec0c869895049c2ce6023c1
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-11 03:06:35 +0000

    sums and averages

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+total_revenue_avg = Store.average(:annual_revenue)
+puts total_revenue_avg
+
+num_over_million = Store.where("annual_revenue > 1000000").count
+puts num_over_million
